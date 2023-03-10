5b3ead9f7b82bab6847e05e58b22c433bcbd233c
online59 <taez.khamrangsi@gmail.com>
2023-03-11 00:14:23 +0700

    Add new project

---
+++ src/test/java/util/CalculatorTest.java
@@ -0,0 +1,89 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void testPowerBasePositivePowerZero() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(5, 0);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testPowerBasePositivePowerOne() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(5, 1);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testPowerBasePositivePowerTwo() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(2, 2);
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    void testPowerBasePositivePowerNegative() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(2, -1);
+
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void testPowerBaseNegativePowerZero() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(-2, 0);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testPowerBaseNegativePowerOne() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(-2, 1);
+
+        assertEquals(-2, result);
+    }
+
+    @Test
+    void testPowerBaseNegativePowerTwo() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(-2, 2);
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    void testPowerBaseNegativePowerOddNumber() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(-2, 3);
+
+        assertEquals(-8, result);
+    }
+
+    @Test
+    void testPowerBaseNegativePowerNegativeNumber() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(-2, -1);
+
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void testPowerBasePositivePowerVeryBigNumber() {
+        Calculator calculator = new Calculator();
+        int result = calculator.power(2, 64);
+        assertNotEquals(0, result);
+
+        // This program cannot deal with a big number
+    }
+}
